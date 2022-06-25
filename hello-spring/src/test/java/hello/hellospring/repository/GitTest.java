47c53fdf11fb7e45468e07c28e64de8779d00adf
Kiseok Lee <dev.leekiseok@gmail.com>
2022-06-25 13:29:34 +0900

    destop commit

---
+++ hello-spring/src/test/java/hello/hellospring/repository/GitTest.java
@@ -0,0 +1,26 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GitTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+    }
+}
